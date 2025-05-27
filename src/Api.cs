acd2b0ca7e146a10dc96ab7a1189644b62685751
Michael Bond <michael@bondcodes.com>
2025-05-27 14:30:22 -0400

    Add source files

---
+++ src/Api.cs
@@ -0,0 +1,13 @@
+﻿#!/usr/bin/env dotnet run
+#:sdk Microsoft.NET.Sdk.Web
+#:property LangVersion preview
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+var name = args.Length > 0 ? args[0] : "world";
+
+app.MapGet("/", () => $"Hello, {name}!");
+
+app.Run();
---
+++ src/Form.cs
@@ -0,0 +1,18 @@
+﻿#!/usr/bin/env dotnet run
+#:property LangVersion preview
+
+#:property UseWindowsForms true
+#:property TargetFramework net10.0-windows
+#:property OutputType WinExe
+
+var name = args.Length > 0 ? args[0] : "world";
+
+Application.Run(
+    new Form
+    {
+        Text = $"Hello, {name}!",
+        Width = 800,
+        Height = 600,
+        StartPosition = FormStartPosition.CenterScreen
+    }
+);
